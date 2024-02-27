6da350a0e1b25f16a83ce5972ef41d728667626e
Erfan Mostafavi <Rfnmostafavi@gmail.com>
2024-02-28 03:09:07 +0330

    DataBase Creatiand applying Migration(Code First)

---
+++ ResumeShop/Data/EshopContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ResumeShop.Models;
+
+namespace ResumeShop.Data
+{
+    public class EshopContext : DbContext
+    {
+        public EshopContext(DbContextOptions<EshopContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Category> categories { get; set; } 
+
+    }
+}
