6d1e1ce70ba3ad9dfb53d60627bdbf0b680afde4
Erfan <Erfan@DESKTOP-B2OLFVO>
2024-02-27 16:15:55 +0330

    Add project files.

---
+++ ResumeShop/Models/Cart.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ResumeShop.Models
+{
+    public class Cart
+    {
+        public Cart()
+        {
+            CartItems = new List<CartItem>();
+        }
+        public int OrderId { get; set; }
+        public List<CartItem> CartItems { get; set; }
+
+        public void AddCartItem(CartItem item)
+        {
+            if (CartItems.Exists(i => i.Item.Id == item.Id))
+            {
+                CartItems.Find(i => i.Item.Id == item.Item.Id)
+                    .Quantity += 1;
+            }
+            else
+            {
+                CartItems.Add(item);    
+            }
+        }
+        public void RemoveCartItem(int itemId)
+        {
+
+            var item = CartItems.SingleOrDefault(c => c.Item.Id == itemId);
+            if (item?.Quantity <= 1)
+            {
+                CartItems.Remove(item);
+            } else if (item!= null)
+            {
+                item.Quantity -= 1;
+            }
+        }
+    }
+}
---
+++ ResumeShop/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace ResumeShop.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public int Description { get; set; }
+    }
+}
