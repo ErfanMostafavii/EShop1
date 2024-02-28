c28d8c65394e77dbdaffe79fcbef027f805aba40
Erfan Mostafavi <Rfnmostafavi@gmail.com>
2024-02-28 15:24:03 +0330

    Adding SeedData

--- ResumeShop/Data/EshopContext.cs
+++ ResumeShop/Data/EshopContext.cs
@@ -5,11 +5,48 @@ namespace ResumeShop.Data
 {
     public class EshopContext : DbContext
     {
-        public EshopContext(DbContextOptions<EshopContext> options):base(options)
+        public EshopContext(DbContextOptions<EshopContext> options) : base(options)
         {
-            
+
         }
-        public DbSet<Category> categories { get; set; } 
+        public DbSet<Category> categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            #region
+
+            modelBuilder.Entity<Category>().HasData(new Category()
+            {
+                Id = 1,
+                Name = "سامسونگ",
+                Description = "لوازم سامسونگ"
+
+            }, new Category()
+            {
+                Id = 2,
+                Name = "هواوی",
+                Description = "لوازم هواوی",
+            }, new Category()
+            {
+                Id = 3,
+                Name = "شیاومی",
+                Description = "لوازم شیایومی",
+            }, new Category()
+            {
+                Id = 4,
+                Name = "اپل",
+                Description = "لوازم اپل",
+            }
+
+
+
+
+
+            );
+
+            #endregion
+            base.OnModelCreating(modelBuilder);
+        }
 
     }
 }
---
+++ ResumeShop/Migrations/20240228115146_SeedData Entering.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ResumeShop.Migrations
+{
+    public partial class SeedDataEntering : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "categories",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "categories",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.InsertData(
+                table: "categories",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, "لوازم سامسونگ", "سامسونگ" },
+                    { 2, "لوازم هواوی", "هواوی" },
+                    { 3, "لوازم شیایومی", "شیاومی" },
+                    { 4, "لوازم اپل", "اپل" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Name",
+                table: "categories",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Description",
+                table: "categories",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
