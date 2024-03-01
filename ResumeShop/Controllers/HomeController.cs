d62fbef63b5dd424869f6c8b8a20ac20cb3ce49c
Erfan Mostafavi <Rfnmostafavi@gmail.com>
2024-03-02 00:54:23 +0330

    Added New Products

--- ResumeShop/Controllers/HomeController.cs
+++ ResumeShop/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Logging;
+using ResumeShop.Data;
 using ResumeShop.Models;
 using System;
 using System.Collections.Generic;
@@ -13,21 +14,27 @@ namespace ResumeShop.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private EshopContext _context { get; set; }
+        public HomeController(ILogger<HomeController> logger, EshopContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var products = _context.Products.ToList();
+            return View(products);
         }
         
 
         public IActionResult Privacy()
         {
             return View();
+        }
+        public IActionResult Detail(int id)
+        {
+            return null;
         }
 
         [Route("ContactUs")]
