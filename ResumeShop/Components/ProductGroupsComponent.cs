a9d0f0f85ecaeba5cae2aff6fedcc9b08e1c880b
Erfan Mostafavi <Rfnmostafavi@gmail.com>
2024-03-04 02:13:36 +0330

    Bringing up Category

---
+++ ResumeShop/Components/ProductGroupsComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ResumeShop.Data;
+using System.Threading.Tasks;
+
+namespace ResumeShop.Components
+{
+    public class ProductGroupsComponent : ViewComponent
+    {
+
+        private EshopContext _context;
+
+
+        public ProductGroupsComponent(EshopContext context)
+        {
+            _context = context;            
+        }
+
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("/Views/Component/ProductGroupsComponent.cshtml", _context.categories);
+        }
+    }
+}
